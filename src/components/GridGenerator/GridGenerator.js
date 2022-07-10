7b607f39e98870a75f46d42f518a82fc55b50d6d
Shubham Kumar <shubhamkumar8032@gmail.com>
2022-07-10 21:21:10 +0530

    Refactoring

--- src/components/GridGenerator/GridGenerator.js
+++ src/components/GridGenerator/GridGenerator.js
@@ -1,10 +1,10 @@
-import { Fragment, useEffect, useRef, useState } from 'react';
+import { Fragment, useCallback, useEffect, useRef, useState } from 'react';
 
 import { kit } from './LoadSound';
 import './GridGenerator.css';
 import Row from './Row';
 
-const LED = ({ cell, playing }) => {
+const LED = ({ cell }) => {
   const on = cell ? 'led led-on' : 'led';
   return (
     <div className="led-container">
@@ -13,46 +13,38 @@ const LED = ({ cell, playing }) => {
   );
 };
 
-const LEDstrip = ({ cellActive, playing }) => {
+const LEDstrip = ({ cellActive }) => {
   return (
     <div className="row-container">
       {cellActive.map((cell, cellIndex) => {
-        return <LED key={cellIndex} cell={cell} playing={playing}/>;
+        return <LED key={cellIndex} cell={cell} />;
       })}
     </div>
   );
 };
 
-const GridGenerator = ({
-  playing,
-  setPlaying,
-  numSteps,
-  tempo,
-  distortionValue,
-  bitCrusherValue,
-  chebyValue,
-  reverbValue
-}) => {
-  const initCellActive = () => Array.from({ length: numSteps }, () => false);
+const GridGenerator = ({ playing, setPlaying, numSteps, tempo }) => {
+  const initCellActive = useCallback(
+    () => Array.from({ length: numSteps }, () => false),
+    [numSteps]
+  );
   const [cellActive, setCellActive] = useState(initCellActive);
 
   // Counter index variable
   const activeCellIndex = useRef(0);
 
-  const [drumkit, _] = useState(
-    Array.from({ length: 9 }, () => false)
-  );
+  const drumkit = useRef(Array.from({ length: 9 }, () => false));
 
   // Reinitialize cellActive state when number of steps changes
   useEffect(() => {
-    if (playing) setPlaying(false);
     activeCellIndex.current = 0;
     setCellActive(initCellActive);
-  }, [numSteps]);
+  }, [numSteps, initCellActive]);
 
   // Counter tick
   // beat duration = (60 seconds / BPM * 4) * 1000 ms
   const interval = useRef(null);
+  const isPlaying = playing === true;
   useEffect(() => {
     if (playing)
       interval.current = setInterval(() => {
@@ -68,22 +60,18 @@ const GridGenerator = ({
       }, parseInt(60000 / (tempo * 4)));
     else clearInterval(interval.current);
     return () => clearInterval(interval.current);
-  }, [playing === true, tempo]);
+  }, [isPlaying, tempo, playing, cellActive, numSteps]);
 
   return (
     <Fragment>
-      <LEDstrip cellActive={cellActive} playing={playing} />
-      {drumkit.map((drum, drumIndex) => {
+      <LEDstrip cellActive={cellActive} />
+      {drumkit.current.map((_, drumIndex) => {
         return (
           <Row
             key={drumIndex}
             cellActive={cellActive}
             numSteps={numSteps}
             instrument={kit[drumIndex]}
-            distortionValue={distortionValue}
-            bitCrusherValue={bitCrusherValue}
-            chebyValue={chebyValue}
-            reverbValue={reverbValue}
           />
         );
       })}
